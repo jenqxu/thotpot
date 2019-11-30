19e46e386b905dc994bd89b8838fcb6cbbd96ce3
Vincent Enierga <venierga@email.unc.edu>
2019-11-30 03:46:18 -0500

    cannot get buttons to align to right side of header :(

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,18 +1,27 @@
-import React from "react";
-import "./Header.css";
+import React, {Component} from 'react';
+import './Header.css';
 import Logo from './logoWithText.svg';
+import {Button, ButtonToolbar, ButtonGroup} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
 
-class Header extends React.Component{
+
+class Header extends Component {
     render(){
         return (
-           <nav className="Nav">
-             <div className="Nav-menus">
-               <div className="Nav-brand">
-                   <img class="logo" src={Logo}/>
-               </div>
-             </div>
-           </nav>
-       );
+          <nav className='Nav'>
+            <div className='Nav-menus'>
+              <ButtonToolbar>
+                <div className='justify-content-between'>
+                <img className = 'logo' alt='thotpot logo' src={Logo}/>
+                <ButtonGroup className='buttons'>
+                  <Button className='headerButton' variant='outline-dark'>Sign up</Button>
+                  <Button className='headerButton' variant='outline-dark'>Log in</Button>
+                </ButtonGroup>
+                </div>
+              </ButtonToolbar>
+            </div>
+          </nav>
+      );
     }   
 }
 export default Header;
